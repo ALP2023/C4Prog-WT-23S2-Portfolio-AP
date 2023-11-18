8946a1d291e3b69d0b4fd0b2073f1dd7a04c6885
April Pickard <aprillpickard@gmail.com>
2023-11-18 18:17:58 +0800

    feat: Phase 5 start - TailwindCSS setup.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      '.index.html',
+      '.phase-*/*.html',
+      '.pages/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
